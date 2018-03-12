ecdcc0bf2c5368b22bd55e3705f60b7715e17cea
Andres Garcia Osorio <andres.garcia.dev@outlook.com>
2018-03-12 17:27:24 +0100

    Design Prototype Login

---
+++ Droid/Renderers/RoundButtonRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(AppTokiota.Droid.Renderers.RoundButtonRenderer))]
+namespace AppTokiota.Droid.Renderers
+{
+    /// <summary>
+    /// Overrides AppCompat problems with round buttons. Works like a charm!
+    /// </summary>
+    public class RoundButtonRenderer : ButtonRenderer
+    {
+        public RoundButtonRenderer(Context context) : base(context)
+        {
+           
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}

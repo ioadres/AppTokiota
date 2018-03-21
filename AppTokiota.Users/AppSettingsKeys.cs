b5af4871e6818d9b3a0d7a065dd26b2b9f8faf1a
Andres <andresprogdev1@gmail.com>
2018-03-21 22:59:00 +0100

    Update AppSettingsKeys.cs

--- AppTokiota.Users/AppSettingsKeys.cs
+++ AppTokiota.Users/AppSettingsKeys.cs
@@ -6,15 +6,15 @@ namespace AppTokiota
     public static partial class AppSettings
     {
         // Endpoints
-        private const string DefaultMicrosoftTenant = "3c7b5863-ed0c-442a-a87b-48c140428bd7";
-        private const string DefaultMicrosoftApiClientId = "d4e094ac-7dc8-4a76-b04f-5774d23b693b";
+        private const string DefaultMicrosoftTenant = "";
+        private const string DefaultMicrosoftApiClientId = "";
         private const string DefaultMicrosoftGrantType = "password";
-        private const string DefaultMicrosoftResource = "https://tokiota.com/sso";
+        private const string DefaultMicrosoftResource = "";
         private const string DefaultMicrosoftAuthEndpoint = "https://login.microsoft.com/{0}/oauth2/token";
 
 
         // Endpoint Timesheet
-        private const string DefaultTimesheetUrlEndPoint = "https://timesheet.tokiota.com//api/timesheets?";
+        private const string DefaultTimesheetUrlEndPoint = "?";
         
 
 

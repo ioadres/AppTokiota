e257d42b6aefa7edf343926ebb5cd395bda5c334
mvazquezalonso <mvazquezalonso@gmail.com>
2018-05-17 00:08:15 +0200

    Component Interfaz Review

--- AppTokiota.Users/Components/Review/ReviewPageViewModel.cs
+++ AppTokiota.Users/Components/Review/ReviewPageViewModel.cs
@@ -5,27 +5,96 @@ using AppTokiota.Users.Models;
 using Prism.Commands;
 using Prism.Navigation;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 
 namespace AppTokiota.Users.Components.Review
 {
-    public class Contacto
+    public class Review
     {
-        public int userId { get; set; }
         public int id { get; set; }
-        public string title { get; set; }
-        public string body { get; set; }
-        
+        public string project { get; set; }
+        public string task { get; set; }
+        public string description { get; set; }
+        public Time schedule { get; set; }
+        public double consumed { get; set; }
+        public double deviate { get; set; }
+        public Time imputation { get; set; }
+        public Time deviation { get; set; }
+
     }
-    public class ReviewPageViewModel :ViewModelBase
+
+    public class ReviewPageViewModel :ViewModelBase, INotifyPropertyChanged
     {
+        #region datapicker
+        public ObservableCollection<object> YearPicker { get; set; }
+
+        public ObservableCollection<object> MonthYearPicker { get; set; }
+        public ObservableCollection<object> MonthPicker { get; set; }
+        private ObservableCollection<object> MonthYearPickerID { get; set; }
+       
+        private object myMonthYearPicker;
+        private object myMonthPicker;
+        private object myYearPicker;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion datapicker
+        /// <summary>
+        /// Gets or sets the Total DeviationTotal 
+        /// </summary>
+        /// <value>The Total DeviationTotal</value>
+        private double _deviationTotal;
+        public double DeviationTotal
+        {
+            get { return _deviationTotal; }
+            set { SetProperty(ref _deviationTotal, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Total Imputed 
+        /// </summary>
+        /// <value>The Total Imputed</value>
+        private double _imputedTotal;
+        public double ImputedTotal
+        {
+            get { return _imputedTotal; }
+            set { SetProperty(ref _imputedTotal, value); }
+        }
+
+
+        public object MyMonthYearPicker
+        {
+            get { return myMonthYearPicker; }
+            set { myMonthYearPicker = value; RaisePropertyChanged("Selected"); }
+        }
+        public object MyMonthPicker
+        {
+            get { return myMonthPicker; }
+            set { myMonthPicker = value; RaisePropertyChanged("Selected"); }
+        }
+        public object MyYearPicker
+        {
+            get { return myYearPicker; }
+            set { myYearPicker = value; RaisePropertyChanged("Selected"); }
+        }
+
+        private ObservableCollection<Review> lstReview;
+        public ObservableCollection<Review> LstReview
+        {
+            get { return lstReview; }
+            set { SetProperty(ref lstReview, value); }
+        }
+
         #region Services
         protected readonly IReviewModule _reviewModule;
         #endregion
+        DateTimeFormatInfo dtinfo = new CultureInfo("en").DateTimeFormat;
 
         #region Construct
         public ReviewPageViewModel(IViewModelBaseModule baseModule, IReviewModule reviewModule) : base(baseModule)
@@ -33,120 +102,48 @@ namespace AppTokiota.Users.Components.Review
             _reviewModule = reviewModule;
 
             Title = "Review";
+            MonthYearPicker = new ObservableCollection<object>();
+            MonthYearPickerID = new ObservableCollection<object>();
 
-            DateTime date = DateTime.Now;
-            _dates = new ObservableCollection<DateTime>();
-            _specialDates = new ObservableCollection<SpecialDate>();
+            MonthPicker = new ObservableCollection<object>();
+            YearPicker = new ObservableCollection<object>();
 
-            var from = new DateTime(date.Year, date.Month, 1);
-            lstContatos = new List<Contacto>();
-            lstContatos.Add(new Contacto
-            { id = 1,userId = 1, body="Descripcion1",title= "Titulo1"
-            });
-            lstContatos.Add(new Contacto
+
+            for (int iyear = DateTime.Now.Year - 3; iyear <= (DateTime.Now.Year + 5); iyear++)
             {
-                id = 2,
-                userId = 2,
-                body = "Descripcion2",
-                title = "Titulo2"
-            });
+                YearPicker.Add(iyear);
+            }
 
+            for (int imes = DateTime.MinValue.Month; imes < DateTime.MaxValue.Month + 1; imes++)
+            {
+                
+                MonthPicker.Add(dtinfo.GetMonthName(imes));
+            }
+
+            //for (int iyear = DateTime.Now.Year - 3; iyear <= (DateTime.Now.Year + 5); iyear++)
+            //{
+            //    for (int imes = DateTime.MinValue.Month; imes < DateTime.MaxValue.Month; imes++)
+            //    {
+            //        MonthYearPicker.Add(imes.ToString() + "-" + iyear.ToString());
+            //    }
+            //}
+
+            myMonthPicker = new ObservableCollection<int>() { DateTime.Today.Month };
+            myYearPicker = new ObservableCollection<object>() { DateTime.Today.Year };
+
+           
+            //LstReview = new ObservableCollection<Review>();
+            //LstReview.Add(new Review { id = 1, project = "Proyecto1", task = "Task1", description = "description1", schedule=new Time {Hour= 2, Minute = 20 },consumed=50,deviate= 3, imputation = new Time { Hour = 2, Minute = 20 }, deviation = new Time { Hour = 2, Minute = 20 } });
+            //LstReview.Add(new Review { id = 2, project = "Proyecto2", task = "Task2", description = "description2", schedule = new Time { Hour = 2, Minute = 20 }, consumed=20,  deviate=5, imputation = new Time { Hour = 2, Minute = 20 }, deviation = new Time { Hour = 2, Minute = 20 } });
         }
-        private Models.TimesheetForDay _currentTimesheetForDay;
-        public Models.TimesheetForDay CurrentTimesheetForDay
-        {
-            get { return _currentTimesheetForDay; }
-        }
+
         #endregion
-        public ObservableCollection<Contacto> lstContactos { get; }
-        private ObservableCollection<DateTime> _dates;
-        public ObservableCollection<DateTime> Dates
-        {
-            get { return _dates; }
-            set
-            {
-                SetProperty(ref _dates, value);
-            }
-        }
 
-        /// <summary>
-        /// Gets or sets the special collection dates 
-        /// </summary>
-        /// <value>The special dates</value>
-        private ObservableCollection<SpecialDate> _specialDates;
-        public ObservableCollection<SpecialDate> SpecialDates
-        {
-            get { return _specialDates; }
-            set { SetProperty(ref _specialDates, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the activities collection dates 
-        /// </summary>
-        /// <value>The activities</value>
-        private ObservableCollection<ActivityDay> _activities;
-        public ObservableCollection<ActivityDay> Activities
-        {
-            get { return _activities; }
-            set { SetProperty(ref _activities, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the Total Deviation 
-        /// </summary>
-        /// <value>The Total Deviation</value>
-        private double _deviation;
-        public double Deviation
-        {
-            get { return _deviation; }
-            set { SetProperty(ref _deviation, value); }
-        }
-
-        /// <summary>
-        /// Gets or sets the Imputed 
-        /// </summary>
-        /// <value>The Imputed</value>
-        private double _imputed;
-        public double Imputed
-        {
-            get { return _imputed; }
-            set { SetProperty(ref _imputed, value); }
-        }
-        
-
-
-
-        public DelegateCommand<object> OnInfoActivityItemCommand => new DelegateCommand<object>((obj) => { OnInfoActivityItem((ActivityDay)obj); });
-
-        protected void OnInfoActivityItem(ActivityDay from)
-        {
-            var navigationParameters = new NavigationParameters();
-            navigationParameters.Add(ActivityDay.Tag, from);
-            BaseModule.NavigationService.NavigateAsync(PageRoutes.GetKey<Timesheet.TimesheetPage>(), navigationParameters, true, true);
-        }
-
-        public override void OnNavigatedTo(NavigationParameters parameters)
-        {
-            var keyContains = parameters.ContainsKey(TimesheetForDay.Tag);
-            if (keyContains)
-            {
-                var currentTimesheetForDay = parameters.GetValue<TimesheetForDay>(TimesheetForDay.Tag);
-                GetMonthOfTimeSheet(currentTimesheetForDay);
-                Title = currentTimesheetForDay.Day.Date.ToString("yyyy-MM-dd");
-            }
-        }
-
-        private void GetMonthOfTimeSheet(TimesheetForDay timesheet)
-        {
-            _currentTimesheetForDay = timesheet;
-
-            Device.BeginInvokeOnMainThread(() =>
-            {
-                Activities = new ObservableCollection<ActivityDay>(timesheet.Activities);
-                Imputed = Activities.Sum(x => x.Imputed);
-                Deviation = Activities.Sum(x => x.Deviation);
-            });
-        }
+        //public void RaisePropertyChanged(string name)
+        //{
+        //    if (PropertyChanged != null)
+        //        PropertyChanged(this, new PropertyChangedEventArgs(name));
+        //}
 
     }
 }

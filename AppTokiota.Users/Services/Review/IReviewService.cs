e15ccaafebfc357525d48d4cf99d68be48c7609f
mvazquezalonso <mvazquezalonso@gmail.com>
2018-05-17 00:09:13 +0200

    Service and Service Fake Review

---
+++ AppTokiota.Users/Services/Review/IReviewService.cs
@@ -0,0 +1,13 @@
+﻿using AppTokiota.Users.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTokiota.Users.Services
+{
+    public interface IReviewService
+    {
+        Task<Review> GetReviewForMonth(DateTime from, DateTime To);
+    }
+}

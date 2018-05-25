02d7ebb497c0fac00e6adf5f54e32204ef19168d
mvazquezalonso <mvazquezalonso@gmail.com>
2018-05-25 21:11:13 +0200

    Timeline format date. Convert Date of week to string

---
+++ AppTokiota.Users/Converters/DateToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppTokiota.Users.Converters
+{
+    public class DateToStringConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = DateTime.Parse(value.ToString());
+            var dayOfWeek = date.DayOfWeek;
+            var dayOfWeekNumber = date.Day;
+
+            var dayOfWeekString = dayOfWeekNumber + "-" + dayOfWeek; 
+            return dayOfWeekString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}

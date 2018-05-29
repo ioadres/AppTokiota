477b3d514d3b8e9a60568b08c67109bc3b759f50
mvazquezalonso <mvazquezalonso@gmail.com>
2018-05-29 12:33:39 +0200

    Disable Viecell Of holidays and Weekend Days

--- AppTokiota.Users/Converters/EnableDayConverter.cs
+++ AppTokiota.Users/Converters/EnableDayConverter.cs
@@ -6,13 +6,13 @@ using AppTokiota.Users.Helpers;
 
 namespace AppTokiota.Users.Converters
 {
-    public class DisableDayConverter :  IValueConverter
+    public class EnableDayConverter :  IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var day = (Day)value;
             var result =  FreeDay.IsFreeOrWeekendDay(day);
-            return result; 
+            return !result; 
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

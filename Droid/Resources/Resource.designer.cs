e5153cc0bda19ae6654e427b4a9e33cc24006644
mvazquezalonso <mvazquezalonso@gmail.com>
2018-06-11 22:27:19 +0200

    Icon square

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -5133,70 +5133,70 @@ namespace AppTokiota.Droid
 			public const int logo_horizontal = 2130837779;
 			
 			// aapt resource value: 0x7f020114
-			public const int menu = 2130837780;
+			public const int logoSplash = 2130837780;
 			
 			// aapt resource value: 0x7f020115
-			public const int mr_button_connected_dark = 2130837781;
+			public const int menu = 2130837781;
 			
 			// aapt resource value: 0x7f020116
-			public const int mr_button_connected_light = 2130837782;
+			public const int mr_button_connected_dark = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int mr_button_connecting_dark = 2130837783;
+			public const int mr_button_connected_light = 2130837783;
 			
 			// aapt resource value: 0x7f020118
-			public const int mr_button_connecting_light = 2130837784;
+			public const int mr_button_connecting_dark = 2130837784;
 			
 			// aapt resource value: 0x7f020119
-			public const int mr_button_dark = 2130837785;
+			public const int mr_button_connecting_light = 2130837785;
 			
 			// aapt resource value: 0x7f02011a
-			public const int mr_button_light = 2130837786;
+			public const int mr_button_dark = 2130837786;
 			
 			// aapt resource value: 0x7f02011b
-			public const int mr_dialog_close_dark = 2130837787;
+			public const int mr_button_light = 2130837787;
 			
 			// aapt resource value: 0x7f02011c
-			public const int mr_dialog_close_light = 2130837788;
+			public const int mr_dialog_close_dark = 2130837788;
 			
 			// aapt resource value: 0x7f02011d
-			public const int mr_dialog_material_background_dark = 2130837789;
+			public const int mr_dialog_close_light = 2130837789;
 			
 			// aapt resource value: 0x7f02011e
-			public const int mr_dialog_material_background_light = 2130837790;
+			public const int mr_dialog_material_background_dark = 2130837790;
 			
 			// aapt resource value: 0x7f02011f
-			public const int mr_group_collapse = 2130837791;
+			public const int mr_dialog_material_background_light = 2130837791;
 			
 			// aapt resource value: 0x7f020120
-			public const int mr_group_expand = 2130837792;
+			public const int mr_group_collapse = 2130837792;
 			
 			// aapt resource value: 0x7f020121
-			public const int mr_media_pause_dark = 2130837793;
+			public const int mr_group_expand = 2130837793;
 			
 			// aapt resource value: 0x7f020122
-			public const int mr_media_pause_light = 2130837794;
+			public const int mr_media_pause_dark = 2130837794;
 			
 			// aapt resource value: 0x7f020123
-			public const int mr_media_play_dark = 2130837795;
+			public const int mr_media_pause_light = 2130837795;
 			
 			// aapt resource value: 0x7f020124
-			public const int mr_media_play_light = 2130837796;
+			public const int mr_media_play_dark = 2130837796;
 			
 			// aapt resource value: 0x7f020125
-			public const int mr_media_stop_dark = 2130837797;
+			public const int mr_media_play_light = 2130837797;
 			
 			// aapt resource value: 0x7f020126
-			public const int mr_media_stop_light = 2130837798;
+			public const int mr_media_stop_dark = 2130837798;
 			
 			// aapt resource value: 0x7f020127
-			public const int mr_vol_type_audiotrack_dark = 2130837799;
+			public const int mr_media_stop_light = 2130837799;
 			
 			// aapt resource value: 0x7f020128
-			public const int mr_vol_type_audiotrack_light = 2130837800;
+			public const int mr_vol_type_audiotrack_dark = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int MyIcon = 2130837801;
+			public const int mr_vol_type_audiotrack_light = 2130837801;
 			
 			// aapt resource value: 0x7f02012a
 			public const int navigation_empty_icon = 2130837802;
